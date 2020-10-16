bc13901631e8aff32cf3327fad4e844da1132da6
Amber Janzen <janzenamber@gmail.com>
2020-10-16 10:41:56 -0400

    updated helpers

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -1,9 +1,12 @@
   let APIURL = '';
+
+
+
 switch(window.location.hostname) {
     case 'localhost' || '127.0.0.1':
         APIURL = 'http://localhost:4000'
         break;
     case 'plant-pal-app.herokuapp.com/':
-        APIURL = ' https://plant-pal-app.herokuapp.com/'
+        APIURL = 'https://my-plant-pal-app.herokuapp.com/'
 }
 export default APIURL;
